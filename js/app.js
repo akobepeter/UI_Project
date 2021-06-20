03b81ddb5cc0e6c76d2a58a5c08daca0d63d1d15
akobepeter <mrpeterakobe@gmail.com>
2021-06-20 17:08:45 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+        // sideNav
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+            });
+    
+              //slider
+            $(document).ready(function(){
+              $('.slider').slider({
+                indicators:false,
+                height:500,
+                transition:500,
+                interval:6000
+              });
+            });
+    
+            //scrollspy
+            $(document).ready(function(){
+            $('.scrollspy').scrollSpy();
+           });
